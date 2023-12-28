098f6dbd35414764b0436fec2096c0b8fe077457
AlexMarrer <95776537+RayMatador@users.noreply.github.com>
2023-12-28 15:20:20 +0100

    asd

--- src/main/java/Pruefung2/Main.java
+++ src/main/java/Pruefung2/Main.java
@@ -1,11 +1,14 @@
 package Pruefung2;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
+import java.util.stream.Collectors;
+
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -16,58 +19,57 @@ import org.apache.solr.common.SolrInputDocument;
 
 public class Main {
 
-	private final static File wordList = new File(
+	private final static Path path = Path.of(
 			"C:\\Users\\alexu\\OneDrive\\eclipse-workspace\\pruefung2\\ichStandUfHaerin\\src\\main\\java\\Pruefung2\\words.txt");
 
-	private final static String solrURL = "http://localhost:8983/solr/solrtest";
+	private final static String solrURL = "http://localhost:8983/solr/SolrTestCore";
 
 	private final static SolrClient solrClient = new HttpSolrClient.Builder(solrURL).build();
 
 	public static void main(String[] args) throws SolrServerException, IOException {
-	
+		long startTime = System.nanoTime();
+
 		createData(getWords());
 		getData();
-		
+
+		long endTime = System.nanoTime();
+		long durationInNano = (endTime - startTime);
+		long durationInMillis = durationInNano / 1_000_000;
+		long durationInSek = durationInNano / 1_000_000_000;
+		System.out.println("Gesamtdauer: " + durationInSek + "s" + "\nGesamtdauer: " + durationInMillis + "milli");
+
 		solrClient.close();
 	}
 
-	private static ArrayList<String> getWords() throws FileNotFoundException {
-		
-		ArrayList<String> words = new ArrayList<>();
-		
-		try (Scanner myScanner = new Scanner(wordList)) {
-			while (myScanner.hasNext()) {
-				words.add(myScanner.next());
-			}
-		}
-		
-		return words;
+	private static List<String> getWords() throws IOException {
+		return Files.lines(path).flatMap(line -> Arrays.stream(line.split("\\s+"))).collect(Collectors.toList());
 	}
-	
+
 	private static void getData() throws SolrServerException, IOException {
 
 		SolrQuery query = new SolrQuery();
-	    query.setQuery("*:*");
-	    query.setRows(5);
-	    
-	    QueryResponse response = solrClient.query(query);
-	    for (SolrDocument doc : response.getResults()) {
-	        System.out.println(doc);
-	    }
+		query.setQuery("*:*");
+		query.setRows(5);
+
+		QueryResponse response = solrClient.query(query);
+		for (SolrDocument doc : response.getResults()) {
+			System.out.println(doc);
+		}
 	}
 
-	private static void createData(ArrayList<String> words) throws SolrServerException, IOException {
+	private static void createData(List<String> words) throws SolrServerException, IOException {
 		Random randomWord = new Random();
+		int oldInt = 10;
 		ArrayList<SolrInputDocument> documents = new ArrayList<SolrInputDocument>();
 		for (int i = 0; i < 10; i++) {
-	        StringBuilder text = new StringBuilder();
-	        StringBuilder title = new StringBuilder();
-	        int randomTextLength = randomWord.nextInt(501) + 1000;
+			StringBuilder text = new StringBuilder();
+			StringBuilder title = new StringBuilder();
+			int randomTextLength = randomWord.nextInt(501) + 1000;
 			for (int j = 0; j < randomTextLength; j++) {
 				String word = words.get(randomWord.nextInt(words.size()));
-	            text.append(word).append(" ");
+				text.append(word).append(" ");
 				if (j < 5) {
-	                title.append(word).append(" ");
+					title.append(word).append(" ");
 				}
 			}
 			Data data = new Data(i, title.toString(), text.toString());
@@ -77,10 +79,12 @@ public class Main {
 			document.addField("text", data.getText());
 
 			documents.add(document);
+			if ((i + 1) % oldInt == 0 || i == 99999) {
+				solrClient.add(documents);
+				solrClient.commit();
+				documents.clear();
+			}
 		}
-		
-		solrClient.add(documents);
-		solrClient.commit();
 	}
 
 }
